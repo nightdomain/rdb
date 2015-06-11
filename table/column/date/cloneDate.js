6e029628f2a2c859b7fd6fa3d35d45f74dd374d0
Lars-Erik Roald <lars.roald@gmail.com>
2015-06-11 21:57:48 +0200

    toISOString not enumerable

--- table/column/date/cloneDate.js
+++ table/column/date/cloneDate.js
@@ -1,11 +1,17 @@
 var toISOString = require('./toISOString');
 
-function cloneDate (date) {
-	date = new Date(date);
-	date.toISOString = function()  {
-		return toISOString(date);
-	};
-	return date;
+function cloneDate(date) {
+    date = new Date(date);
+
+    Object.defineProperty(date, 'toISOString', {
+        enumerable: false,
+        configurable: true,
+        writable: true,
+        value: function() {
+            return toISOString(date);
+        }
+    });
+    return date;
 }
 
 module.exports = cloneDate;
